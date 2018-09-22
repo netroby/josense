bf754dcf86adb9799306e7c73e7a601ee00b1ad9
ZhiFeng Hu <hufeng1987@gmail.com>
2018-09-22 21:49:24 +0800

    Add home.hbs

---
+++ src/main/kotlin/com/netroby/josense/controllers/HomeController.kt
@@ -0,0 +1,12 @@
+package com.netroby.josense.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+open class HomeController {
+    @GetMapping("/")
+    fun home(): String {
+        return "home/home"
+    }
+}
