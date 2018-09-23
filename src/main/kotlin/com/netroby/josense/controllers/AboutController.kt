620bab5a9d8f8e064c5dc12730b26f421fc29537
ZhiFeng Hu <hufeng1987@gmail.com>
2018-09-23 10:31:35 +0800

    Import template from gosense

--- src/main/kotlin/com/netroby/josense/controllers/AboutController.kt
+++ src/main/kotlin/com/netroby/josense/controllers/AboutController.kt
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-open class HomeController {
-    @GetMapping("/")
+open class AboutController {
+    @GetMapping("/about")
     fun home(): String {
-        return "home"
+        return "about"
     }
 }
