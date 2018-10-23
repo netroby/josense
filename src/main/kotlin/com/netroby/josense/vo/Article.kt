1bc874321d226d3ef3cb0cad4f4b03328dce9663
huzhifeng <hufeng1987@gmail.com>
2018-10-23 10:21:38 +0800

    Upgrade article.kt data  class

--- src/main/kotlin/com/netroby/josense/vo/Article.kt
+++ src/main/kotlin/com/netroby/josense/vo/Article.kt
@@ -2,15 +2,19 @@ package com.netroby.josense.vo
 
 import lombok.Data
 import lombok.ToString
+import java.io.Serializable
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 @ToString
 @Data
-class Article {
-    @Id
-    val Aid: Long? = null;
-    val Title: String = "";
+class Article : Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    var aid: Long = 0;
+    var title: String = "";
+    var content: String = "";
 
 }
