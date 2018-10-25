fccaa6c0e463ac4364a553038762d30312c638e1
huzhifeng <hufeng1987@gmail.com>
2018-10-25 09:24:01 +0800

    尝试使用分页算法

--- src/main/kotlin/com/netroby/josense/controllers/AdminController.kt
+++ src/main/kotlin/com/netroby/josense/controllers/AdminController.kt
@@ -1,13 +1,23 @@
 package com.netroby.josense.controllers
 
+import com.netroby.josense.repository.ArticleRepository
+import com.netroby.josense.vo.Article
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+
 
 @Controller
-open class AdminController {
+open class AdminController (@Autowired private val articleRepository: ArticleRepository){
     @GetMapping("/admin")
-    fun home(): String {
-        return "admin"
+    fun home(@RequestParam(value = "page") page: Int): Page<Article> {
+        val sort = Sort(Sort.Direction.DESC, "aid")
+        val pageable = PageRequest.of(page, 15, sort)
+        return articleRepository.findAll(pageable)
     }
     @GetMapping("/admin/list")
     fun list(): String {
--- src/main/kotlin/com/netroby/josense/vo/Article.kt
+++ src/main/kotlin/com/netroby/josense/vo/Article.kt
@@ -13,8 +13,10 @@ import javax.persistence.Id
 @Data
 class Article : Serializable {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
-    var aid: Long = 0;
-    var title: String = "";
-    var content: String = "";
-
+    var aid: Long = 0
+    var title: String = ""
+    var content: String = ""
+    var publishStatus: Long = 0
+    var publishTime: Long = 0
+    var views: Long = 0
 }
