9bbd29ac903e7363563f13699241d2805d622423
ZhiFeng Hu <hufeng1987@gmail.com>
2018-10-29 11:03:26 +0800

    Optimize import

--- src/main/kotlin/com/netroby/josense/controllers/AdminController.kt
+++ src/main/kotlin/com/netroby/josense/controllers/AdminController.kt
@@ -1,19 +1,14 @@
 package com.netroby.josense.controllers
 
-import com.alibaba.fastjson.JSON
 import com.netroby.josense.repository.ArticleRepository
-import com.netroby.josense.vo.Article
-import lombok.extern.log4j.Log4j
-import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Page
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
 
 
@@ -21,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView
 open class AdminController (@Autowired private val articleRepository: ArticleRepository){
     private val logger = LoggerFactory.getLogger("admin")
     @GetMapping("/admin")
-    fun home(model: Model, @RequestParam(value = "page") page: Int): ModelAndView {
+    fun home(model: Model, @RequestParam(value = "page", defaultValue = "0") page: Int): ModelAndView {
         val sort = Sort(Sort.Direction.DESC, "aid")
         val pageable = PageRequest.of(page, 15, sort)
         val result =  articleRepository.findAll(pageable)
