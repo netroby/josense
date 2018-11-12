e06ba5b34c10da24c449fbacf8e0e0a5dec705b8
huzhifeng <hufeng1987@gmail.com>
2018-11-12 09:16:10 +0800

    Do not track bin folder

--- bin/main/com/netroby/josense/repository/ArticleRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.netroby.josense.repository
-
-
-import com.netroby.josense.vo.Article
-import org.springframework.data.repository.PagingAndSortingRepository
-
-public interface ArticleRepository : PagingAndSortingRepository<Article, Long> {
-
-}
