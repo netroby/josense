44e27891a0a887274bad59959d5d23e3dd67bf5e
ZhiFeng Hu <hufeng1987@gmail.com>
2018-11-17 19:17:31 +0800

    Using mysql replace postgresql

--- sql/josense.sql
+++ sql/josense.sql
@@ -1,15 +1,11 @@
-create table jo_article
-(
-  aid            bigserial not null
-    constraint jo_article_pkey
-    primary key,
-  title          varchar(65536),
-  content        text,
-  publish_status integer,
-  publish_time   bigint,
-  views          bigint
-);
-
-alter table jo_article
-  owner to postgres;
-
+use josense;
+CREATE TABLE `jo_article` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 DEFAULT '',
+  `content` longtext CHARACTER SET utf8,
+  `publish_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `publish_status` tinyint(1) DEFAULT '1',
+  `views` int(11) DEFAULT '1',
+  PRIMARY KEY (`aid`),
+  FULLTEXT KEY `content` (`title`,`content`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
